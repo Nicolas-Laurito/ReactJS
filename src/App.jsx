27081a639b03b79ae89432da3575c45cc419f175
Nicolas-Laurito <nlaurito@hotmail.com>
2022-08-15 22:41:26 -0300

    Agregando Menu

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,46 @@ import './App.css';
 
 function App() {
   return (
-    <div className="estilos">
-      <h1>Ecommerce Nicolas Laurito</h1>
+    <div>
+     <nav className="navbar navbar-expand-lg navFondo">
+  <div className="container-fluid">
+       <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon" />
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">
+            Inicio
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+            Tipo de Producto1
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+          Tipo de Producto2
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">
+          Tipo de Producto3
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
     </div>
   );
 }
