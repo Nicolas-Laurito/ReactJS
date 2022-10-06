4a81066646589a5b8839eb2a62b4d206d684bf89
Nicolas-Laurito <105505186+Nicolas-Laurito@users.noreply.github.com>
2022-10-06 16:19:55 -0300

    Update Home.jsx

--- src/componentes/PaginaInicio/Home.jsx
+++ src/componentes/PaginaInicio/Home.jsx
@@ -5,32 +5,6 @@ import './Home.css'
 const Home = () => {
     return (
 <>
-  {/* <div id="carouselExampleIndicators" className="carousel slide sec1" data-ride="carousel">
-    <ol className="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to={0} className="active"/>
-      <li data-target="#carouselExampleIndicators" data-slide-to={1} />
-      <li data-target="#carouselExampleIndicators" data-slide-to={2} />
-    </ol>
-    <div className="carousel-inner">
-      <div className="carousel-item active">
-        <img src="./img/imagen1.jpg" />
-      </div>
-      <div className="carousel-item">
-        <img src="./img/imagen2.jpg" />
-      </div>
-      <div className="carousel-item">
-        <img src="./img/imagen3.jpg" />
-      </div>
-    </div>
-    <button className="carousel-control-prev" type="button" data-target="#carouselExampleIndicators" data-slide="prev">
-      <span className="carousel-control-prev-icon" aria-hidden="true"/>
-      <span className="sr-only">Previous</span>
-    </button>
-    <button className="carousel-control-next" type="button" data-target="#carouselExampleIndicators" data-slide="next">
-      <span className="carousel-control-next-icon" aria-hidden="true" />
-      <span className="sr-only">Next</span>
-    </button>
-  </div> */}
 
 
 <div id="carouselExampleIndicators" className="carousel slide sec1"  data-bs-ride="true">
